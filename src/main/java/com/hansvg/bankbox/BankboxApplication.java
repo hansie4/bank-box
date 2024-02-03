10303af6e96c03ae5164577f92576d52c5d2e8b2
Hans Von Gruenigen <hans.vongruenigen@gmail.com>
2024-02-03 12:52:46 -0500

    Initial commit

---
+++ src/main/java/com/hansvg/bankbox/BankboxApplication.java
@@ -0,0 +1,13 @@
+package com.hansvg.bankbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hansvg/bankbox/BankboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansvg.bankbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
